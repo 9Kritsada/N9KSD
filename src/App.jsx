1520f5a9a87153f38f03bfa7ba48dfe02af87c5f
9Kritsada <kritsada.wiriya@gmail.com>
2023-06-12 23:00:57 +0700

    fix darkMode false to true

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Footer from "./components/footer";
 import Data from "./components/datain";
 
 function App() {
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(true);
   return (
     <div className={darkMode ? "dark" : ""}>
       <div className="min-h-screen px-10 bg-zinc-300 dark:text-white dark:bg-gray-900 animation-fade md:px-20">
