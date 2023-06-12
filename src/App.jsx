9f01f52190e30765adcee493910029addec9e439
9Kritsada <kritsada.wiriya@gmail.com>
2023-06-12 22:59:16 +0700

    update app.jsx and footer.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,10 @@ import Footer from "./components/footer";
 import Data from "./components/datain";
 
 function App() {
-  const [darkMode, setDarkMode] = useState(true);
+  const [darkMode, setDarkMode] = useState(false);
   return (
     <div className={darkMode ? "dark" : ""}>
-      <div className={`min-h-screen px-10 bg-gray-300 dark:text-white dark:bg-gray-900 animation-fade`}>
+      <div className="min-h-screen px-10 bg-zinc-300 dark:text-white dark:bg-gray-900 animation-fade md:px-20">
         
         <header className="flex justify-between py-10 items-center">
           <logo className="text-xl font-orbit">N9KSD</logo>
--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,9 +1,13 @@
 const footer = () => {
   return (
-    <footer className="sticky top-[100%] text-center">
-      2023 <span className="font-orbit">N9KSD</span>
+    <footer className="fixed bottom-0 right-0 text-sm font-bold">
+      <div className="">
+        <h1 className="vertical-text py-3">
+          <span className="font-orbit">N9KSD</span> &copy; 2023
+        </h1>
+      </div>
     </footer>
-  )
-}
+  );
+};
 
 export default footer;
